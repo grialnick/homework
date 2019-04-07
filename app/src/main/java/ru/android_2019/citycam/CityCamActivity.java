b246626cd8fd58b7e74bbaa881ba83026276f627
TuzhMS <tuzhilkin.michael.1995@yandex.ru>
2019-04-08 00:52:20 +0300

    Задание с камерой готово

--- app/src/main/java/ru/android_2019/citycam/CityCamActivity.java
+++ app/src/main/java/ru/android_2019/citycam/CityCamActivity.java
@@ -1,13 +1,26 @@
 package ru.android_2019.citycam;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 import ru.android_2019.citycam.model.City;
+import ru.android_2019.citycam.webcams.Webcams;
 
 /**
  * Экран, показывающий веб-камеру одного выбранного города.
@@ -24,6 +37,7 @@ public class CityCamActivity extends AppCompatActivity {
 
     private ImageView camImageView;
     private ProgressBar progressView;
+    private DownloadTask downloadTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +59,181 @@ public class CityCamActivity extends AppCompatActivity {
 
         // Здесь должен быть код, инициирующий асинхронную загрузку изображения с веб-камеры
         // в выбранном городе.
+        Log.i(EXTRA_CITY, city.toString());
+
+        if (savedInstanceState != null) {
+            downloadTask = (DownloadTask) getLastCustomNonConfigurationInstance();
+        }
+        if (downloadTask == null) {
+            downloadTask = new DownloadTask(this);
+            try {
+                downloadTask.execute(city);
+            } catch (Exception e) {
+                Log.e(EXTRA_CITY, "Запрос валится", e);
+            }
+        } else {
+            downloadTask.attachActivity(this);
+        }
+
+    }
+
+    @Override
+    public Object onRetainCustomNonConfigurationInstance() {
+        downloadTask.attachActivity(null);
+        return downloadTask;
     }
 
     private static final String TAG = "CityCam";
+
+    private interface GetResult<T> {
+        T getResult(JsonReader jsonReader) throws IOException;
+    }
+
+    public class DownloadTask extends AsyncTask<City, Void, Bitmap> {
+        private static final String TAG = "DownloadTask";
+
+        private CityCamActivity activity;
+        private Bitmap bitmap;
+
+        void attachActivity(CityCamActivity activity) {
+            this.activity = activity;
+            updateView();
+        }
+
+        void updateView() {
+            if (activity != null) {
+                activity.camImageView.setImageBitmap(bitmap);
+                activity.progressView.setVisibility(View.INVISIBLE);
+                Log.i(TAG, "Картинка установлена");
+            }
+        }
+
+        public DownloadTask(CityCamActivity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        protected Bitmap doInBackground(City... city) {
+            try {
+                Bitmap bitmap;
+                for (int radius = 50; (bitmap = download(city[0], radius)) == null; radius *= 2);
+                return bitmap;
+            } catch (Exception e) {
+                Log.e(TAG, "Проблемы со скачкой", e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            this.bitmap = bitmap;
+//            activity.camImageView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+            updateView();
+        }
+
+        private Bitmap download(City city, int radius) throws IOException {
+            URL url = Webcams.createNearbyUrl(city.latitude, city.longitude, radius);
+            HttpURLConnection urlConnection = null;
+            InputStream in = null;
+
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty("X-RapidAPI-Key",
+                        "6b0f0c636emshbd6f1c732d3dafep15e2d6jsn21866776fb55");
+                urlConnection.connect();
+
+                int code = urlConnection.getResponseCode();
+                if (code != HttpURLConnection.HTTP_OK) {
+                    throw new IOException("Код ответа: " + code);
+                }
+
+                in = urlConnection.getInputStream();
+                JsonReader jsonReader = new JsonReader(new InputStreamReader(in));
+
+                // Получение урла картинки
+                URL webcamUrl = new URL(readWebcamUrl(jsonReader));
+                Log.d(TAG, "Url: " + webcamUrl + " Radius: " + radius);
+                jsonReader.close();
+                in.close();
+
+                urlConnection.disconnect();
+                urlConnection = (HttpURLConnection) webcamUrl.openConnection();
+                urlConnection.setRequestMethod("GET");
+                in = urlConnection.getInputStream();
+                code = urlConnection.getResponseCode();
+                if (code != HttpURLConnection.HTTP_OK) {
+                    throw new IOException("Код ответа: " + code);
+                }
+                BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+                Bitmap bitmap = BitmapFactory.decodeStream(in);
+
+                return bitmap;
+            } catch (IOException e) {
+                Log.e(TAG, "Не получилось скачать", e);
+                return null;
+            } finally {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, "Не удалось завершить InputStream", e);
+                    }
+                }
+
+                urlConnection.disconnect();
+            }
+        }
+
+        private <T> T readJson(JsonReader jsonReader, String[] path, int i, GetResult<T> getResult) throws IOException {
+            T result = null;
+            jsonReader.beginObject();
+            while (jsonReader.hasNext()) {
+                String name = jsonReader.nextName();
+                if (name.equals(path[i])) {
+                    if (path.length - 1 == i) {
+                        result = getResult.getResult(jsonReader);
+                    } else {
+                        result = readJson(jsonReader, path, ++i, getResult);
+                    }
+                } else {
+                    jsonReader.skipValue();
+                }
+            }
+            jsonReader.endObject();
+            return result;
+        }
+
+        private String readWebcamUrl(final JsonReader jsonReader) throws IOException {
+            final String[] path = {"result", "webcams"};
+
+            try {
+                final String webcam = readJson(jsonReader, path, 0, new GetResult<String>() {
+                    @Override
+                    public String getResult(JsonReader jsonReader) throws IOException {
+                        List<String> webcamsUrl = new ArrayList<>();
+
+                        jsonReader.beginArray();
+
+                        while (jsonReader.hasNext()) {
+                            String[] imgPath = {"image", "current", "preview"};
+                            webcamsUrl.add(readJson(jsonReader, imgPath, 0, new GetResult<String>() {
+                                @Override
+                                public String getResult(JsonReader jsonReader) throws IOException {
+                                    return jsonReader.nextString();
+                                }
+                            }));
+                        }
+
+                        jsonReader.endArray();
+                        return webcamsUrl.size() > 0 ? webcamsUrl.get(0) : null;
+                    }
+                });
+                return webcam;
+            } catch (IOException e) {
+                throw new IOException(e);
+            }
+        }
+    }
 }
